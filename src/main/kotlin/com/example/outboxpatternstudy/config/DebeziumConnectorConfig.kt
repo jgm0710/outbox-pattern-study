1c3d5617b841ce2e2966d2e0f83c66e2a35a94d3
jeong-gumin <jgm@paytalab.com>
2025-04-12 18:35:42 +0900

    feat : 디비지움 아웃박스 패턴 구현

--- src/main/kotlin/com/example/outboxpatternstudy/config/DebeziumConnectorConfig.kt
+++ src/main/kotlin/com/example/outboxpatternstudy/config/DebeziumConnectorConfig.kt
@@ -1,7 +1,6 @@
 package com.example.outboxpatternstudy.config
 
-import io.debezium.config.Configuration
-import io.debezium.embedded.Connect
+import io.debezium.engine.ChangeEvent
 import io.debezium.engine.DebeziumEngine
 import io.debezium.engine.format.Json
 import jakarta.annotation.PostConstruct
@@ -9,75 +8,82 @@ import jakarta.annotation.PreDestroy
 import org.apache.kafka.connect.storage.FileOffsetBackingStore
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
+import java.util.Properties
 import java.util.concurrent.Executors
 
 @Component
 class DebeziumConnectorConfig(
     @Value("\${debezium.database.hostname}")
     private val databaseHost: String,
-    
+
     @Value("\${debezium.database.port}")
     private val databasePort: String,
-    
+
     @Value("\${debezium.database.user}")
     private val databaseUser: String,
-    
+
     @Value("\${debezium.database.password}")
     private val databasePassword: String,
-    
+
     @Value("\${debezium.database.server.name}")
     private val databaseServerName: String,
-    
+
     @Value("\${debezium.topic.prefix}")
     private val topicPrefix: String
 ) {
     private val logger = LoggerFactory.getLogger(DebeziumConnectorConfig::class.java)
-    private lateinit var debeziumEngine: DebeziumEngine<Any, Any>
-    
+    private lateinit var debeziumEngine: DebeziumEngine<ChangeEvent<String,String>>
+
     @PostConstruct
     fun start() {
         logger.info("Starting Debezium connector...")
-        
-        val config = Configuration.create()
-            .with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
-            .with("offset.storage", FileOffsetBackingStore::class.java.name)
-            .with("offset.storage.file.filename", "./offsets.dat")
-            .with("offset.flush.interval.ms", "60000")
-            .with("name", databaseServerName)
-            .with("database.hostname", databaseHost)
-            .with("database.port", databasePort)
-            .with("database.user", databaseUser)
-            .with("database.password", databasePassword)
-            .with("database.dbname", "outbox_pattern_study")
-            .with("plugin.name", "pgoutput")
-            .with("table.include.list", "public.outbox")
-            .with("schema.include.list", "public")
-            .with("topic.prefix", topicPrefix)
-            .with("transforms", "outbox")
-            .with("transforms.outbox.type", "io.debezium.transforms.outbox.EventRouter")
-            .with("transforms.outbox.route.by.field", "event_type")
-            .with("transforms.outbox.table.field.event.id", "id")
-            .with("transforms.outbox.table.field.event.key", "id")
-            .with("transforms.outbox.table.field.event.payload", "payload")
-            .with("transforms.outbox.table.field.event.timestamp", "created_at")
-            .build()
-            
+
+        val props = Properties().apply {
+            // Connector 클래스 및 기본 설정
+            setProperty("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
+            setProperty("offset.storage", FileOffsetBackingStore::class.java.name)
+            setProperty("offset.storage.file.filename", "./offsets.dat")
+            setProperty("offset.flush.interval.ms", "60000")
+            setProperty("name", databaseServerName)
+
+            // 데이터베이스 연결 설정
+            setProperty("database.hostname", databaseHost)
+            setProperty("database.port", databasePort)
+            setProperty("database.user", databaseUser)
+            setProperty("database.password", databasePassword)
+            setProperty("database.dbname", "outbox_pattern_study")
+            setProperty("plugin.name", "pgoutput")
+
+            // 테이블 및 스키마 설정
+            setProperty("table.include.list", "public.outbox")
+            setProperty("schema.include.list", "public")
+            setProperty("topic.prefix", topicPrefix)
+
+            // Outbox 변환 설정
+            setProperty("transforms", "outbox")
+            setProperty("transforms.outbox.type", "io.debezium.transforms.outbox.EventRouter")
+            setProperty("transforms.outbox.route.by.field", "event_type")
+            setProperty("transforms.outbox.table.field.event.id", "id")
+            setProperty("transforms.outbox.table.field.event.key", "id")
+            setProperty("transforms.outbox.table.field.event.payload", "payload")
+            setProperty("transforms.outbox.table.field.event.timestamp", "created_at")
+        }
+
         debeziumEngine = DebeziumEngine.create(Json::class.java)
-            .using(config)
+            .using(props)
             .notifying { record ->
                 logger.info("Received change event: $record")
                 // 여기서 Kafka로 이벤트를 전송하거나 다른 처리를 수행할 수 있습니다.
             }
             .build()
-            
+
         val executor = Executors.newSingleThreadExecutor()
         executor.execute(debeziumEngine)
-        
+
         logger.info("Debezium connector started")
     }
-    
+
     @PreDestroy
     fun stop() {
         if (::debeziumEngine.isInitialized) {
